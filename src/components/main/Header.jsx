0f90e2ab565ba807be361546f29a6144dd183192
MOHAMAD-ZUBI <mozubi128@hotmail.com>
2024-09-02 02:32:09 +0300

    chore: Update link in Header component to include full width

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
     <div className="sticky top-0 flex justify-center items-center z-50">
       <div className="max-w-[1000px] max-md:max-w-[358px] mx-auto w-full  bg-gray-200/30 dark:bg-black/30 backdrop-blur-sm p-4 rounded-3xl max-md:mt-[10px] mt-[35px]">
         <div className="flex flex-row justify-between items-center w-full max-md:-mb-3 tracking-wider text-lg">
-          <Link href={"/"}>
+          <Link href={"/"} className="w-full">
             <h1 className="w-full tracking-wider md:font-[600]  dark:text-gray-200">
               Mohamad Zubi
             </h1>
