8ae7c08591ba7cd2535099788f606e0641ec3283
MOHAMAD-ZUBI <mozubi128@hotmail.com>
2024-09-02 02:30:24 +0300

    chore: Add link to homepage in Header component

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -4,6 +4,7 @@ import { WiMoonAltWaningGibbous6 } from "react-icons/wi";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { MdOutlineWbSunny } from "react-icons/md";
 import { setCookie, getCookie } from "cookies-next";
+import Link from "next/link";
 
 const Header = () => {
   const [theme, setTheme] = useState("dark");
@@ -27,9 +28,11 @@ const Header = () => {
     <div className="sticky top-0 flex justify-center items-center z-50">
       <div className="max-w-[1000px] max-md:max-w-[358px] mx-auto w-full  bg-gray-200/30 dark:bg-black/30 backdrop-blur-sm p-4 rounded-3xl max-md:mt-[10px] mt-[35px]">
         <div className="flex flex-row justify-between items-center w-full max-md:-mb-3 tracking-wider text-lg">
-          <h1 className="w-full tracking-wider md:font-[600]  dark:text-gray-200">
-            Mohamad Zubi
-          </h1>
+          <Link href={"/"}>
+            <h1 className="w-full tracking-wider md:font-[600]  dark:text-gray-200">
+              Mohamad Zubi
+            </h1>
+          </Link>
           {/* Large screen bar start*/}
           <div className="flex flex-row max-md:hidden justify-end gap-12 w-full items-center">
             <a href="#about">
